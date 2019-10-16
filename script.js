fea389473b2158b3450365c582a1f7f2fd107f41
tim1357-byte <55963434+tim1357-byte@users.noreply.github.com>
2019-10-15 19:53:29 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -11,3 +11,26 @@
 </ol>
 <img src="${}">
 */
+
+
+window.addEventListener("load", function() {
+   // fetch("https://handlers.education.launchcode.org/static/weather.json").then( function(response) {
+           fetch("https://handlers.education.launchcode.org/static/planets.json").then( function(response) {
+              response.json().then( function(json) {
+                 const div = document.getElementById("missionTarget");
+                 // Add HTML that includes the JSON data
+                 div.innerHTML = `
+                    <ol>
+                       <li>Name: ${json[0].name}</li>
+                       <li>Diameter: ${json[0].diameter}</li>
+                       <li>Star: ${json[0].star}</li>
+                       <li>Distance From Earth: ${json[0].distance}</li>
+                       <li>Number of Moons: ${json[0].moons}</li>
+                    </ol>
+                    <img src="${json[0].image}">
+                 `;
+              });
+         
+           });
+        });
+
